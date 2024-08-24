810eace3b91d404d15814a992e53b9e2aa02f9af
Jonathan <many.d.r.o.2001@hotmail.com>
2024-08-23 22:01:15 -0500

    Integration of socket io into the frontend

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
 const path = require('path');
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
+const server = http.createServer(app);
+const io = socketio(server)
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`));
+
+io.on('connection', socket => console.log('New Connection WS'));
+
+server.listen(PORT, () => console.log(`Server Running on port: ${PORT}`));
